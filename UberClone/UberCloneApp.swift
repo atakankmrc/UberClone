ab3c1ea85b0238fd4682618e28a84a757a4236b1
Atakan KÖMÜRCÜ <komurcu508@gmail.com>
2023-02-05 04:12:15 +0300

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Atakan Kömürcü on 5.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
